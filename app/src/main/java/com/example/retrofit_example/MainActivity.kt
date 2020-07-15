ea6fcf7780f95f88f6ab3e5c667994881210c228
Dream-By <dzmitry.dzianisau@gmail.com>
2020-07-15 16:09:03 +0300

    add onResponse, onFailure

--- app/src/main/java/com/example/retrofit_example/MainActivity.kt
+++ app/src/main/java/com/example/retrofit_example/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.retrofit_example
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.retrofit_example.data.Book
 import com.example.retrofit_example.data.Books
+import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -18,15 +22,25 @@ class MainActivity : AppCompatActivity() {
         val request = ServiceBuilder.buildService(ApiInterface::class.java)
         val call = request.getBooks()
         call.enqueue(object : Callback<List<Books>> {
-            override fun onFailure(call: Call<List<Books>>, t: Throwable) {
-                Toast.makeText(this@MainActivity, "${t.message}", Toast.LENGTH_SHORT).show()
-            }
 
             override fun onResponse(call: Call<List<Books>>, response: Response<List<Books>>) {
-                TODO("Not yet implemented")
+                if (response.isSuccessful) {
+                    Toast.makeText(this@MainActivity, "OK", Toast.LENGTH_SHORT).show()
+                    val bookArrayList: List<Books>? = response.body()
+                    if (bookArrayList != null) {
+                        Handler(Looper.getMainLooper()).post {
+                            //textView.text = bookArrayList.get(i).books.joinToString("\n")
+                            textView.text = bookArrayList.map { Book::title }.toString()
+                        }
+                    } else Toast.makeText(this@MainActivity, "ArrayList is Null", Toast.LENGTH_SHORT).show()
+                }
+                else Toast.makeText(this@MainActivity, "Response is Not Successful", Toast.LENGTH_SHORT).show()
             }
 
-        })
+                override fun onFailure(call: Call<List<Books>>, t: Throwable) {
+                    Toast.makeText(this@MainActivity, "${t.message}", Toast.LENGTH_SHORT).show()
+                }
+            })
 
     }
 }
